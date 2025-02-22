e69ec07e2110a48520726c6a001ab8c4b052eeed
ayoolaowoilu <ayoolaowoilu@gmail.com>
2025-02-22 03:53:42 +0100

    Initial commit

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,14 @@
 import express from "express"
 import router from "./main1.js"
-import cros from "cros"
+import cors from "cors"
 
 const app = express()
-app.use(cros())
+app.use(cors())
 app.use(express.json())
 app.use("/auth",router)
+app.get("/",(req,res)=>{
+    res.send("<h1>Welcome .........</h1>")
+})
 
 app.listen(1234,()=>{
     console.log("Listening at port 1234")
