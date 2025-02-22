078b9c891c2430f281a149d5d7113fb643a6f313
ayoolaowoilu <ayoolaowoilu@gmail.com>
2025-02-22 04:45:27 +0100

    int

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,13 @@
 import express from "express"
 import router from "./main1.js"
-import cors from "cros"
+import cors from "cors"
 
 const app = express()
 app.use(cors())
 app.use(express.json())
 app.use("/auth",router)
 app.get("/",(req,res)=>{
-    res.send("<h1>Welcome .........</h1>")
+    res.send("<h1>Welcome Khaleed how ya .........</h1>")
 })
 
 app.listen(1234,()=>{
