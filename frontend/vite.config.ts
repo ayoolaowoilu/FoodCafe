26f5dc2634a659fb1e27e619be25d8ee46d7220b
ayoolaowoilu <ayoolaowoilu@gmail.com>
2025-02-22 11:58:59 +0100

    ggs

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,11 +3,5 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "./", // Ensures relative paths for assets
-  build: {
-    outDir: "dist"
-  },  server: {
-    port: 5173
-  }
+  plugins: [react()]
 })
