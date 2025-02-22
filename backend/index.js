bec6ec991173acc8bc09b127c8e8b2af9e632b17
ayoolaowoilu <ayoolaowoilu@gmail.com>
2025-02-22 04:58:12 +0100

    int

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,11 @@ import router from "./main1.js"
 import cors from "cors"
 
 const app = express()
-app.use(cors())
+app.use(cors({
+    origin:"http://localhost:5173",
+    methods: "GET,POST,PUT,DELETE",
+    credentials: true
+  }))
 app.use(express.json())
 app.use("/auth",router)
 app.get("/",(req,res)=>{
