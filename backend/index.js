8c4e16f075b41d5e0abbf5d377590b0f4822fae2
ayoolaowoilu <ayoolaowoilu@gmail.com>
2025-02-25 15:57:08 +0100

    LOl

--- backend/index.js
+++ backend/index.js
@@ -4,29 +4,34 @@ import router from "./main1.js"
 
 const app = express();
 
-app.use(cors(
-    {
-                origin: "https://food-cafe-homeoffoods.netlify.app/", 
-            methods: ["GET", "POST", "PUT", "DELETE","OPTIONS"], 
-            allowedHeaders: ["Content-Type", "Authorization"],
-            credentials: true
-      }
-));
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "https://food-cafe-homeoffoods.netlify.app/");
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+app.use(
+    cors({
+      origin: "https://food-cafe-homeoffoods.netlify.app", 
+      methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+      allowedHeaders: ["Content-Type", "Authorization"],
+      credentials: true,
+    })
+  );
   
 
+  app.use(express.json());
+  
+
+  app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "https://food-cafe-homeoffoods.netlify.app");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    res.header("Access-Control-Allow-Credentials", "true");
+  
     if (req.method === "OPTIONS") {
-      return res.status(200).end();
+      return res.sendStatus(200);
     }
   
     next();
   });
 
 
-app.use(express.json());
+
 app.use("/auth", router);
 app.get("/", (req, res) => {
     res.send("<h1>Welcome Khaleed, how ya .........</h1>");
