4a521d12c50b7e1100c45be57e45bf4850ce966c
ayoolaowoilu <ayoolaowoilu@gmail.com>
2025-02-25 15:49:16 +0100

    ue

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(cors(
     {
                 origin: "https://food-cafe-homeoffoods.netlify.app/", 
-            methods: ["GET", "POST", "PUT", "DELETE"], 
+            methods: ["GET", "POST", "PUT", "DELETE","OPTIONS"], 
             allowedHeaders: ["Content-Type", "Authorization"],
             credentials: true
       }
