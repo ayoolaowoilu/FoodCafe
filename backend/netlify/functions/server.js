98cd4999df7ec535dad761925c054b1f1a3aae47
ayoolaowoilu <ayoolaowoilu@gmail.com>
2025-02-27 00:26:12 +0100

    netlify

--- backend/netlify/functions/server.js
+++ backend/netlify/functions/server.js
@@ -5,12 +5,15 @@ import router from "./main1.js";
 
 const app = express();
 
-app.use(cors());
-app.use(express.json());
 
+app.use(cors({ origin: "*", methods: "GET,POST,PUT,DELETE,OPTIONS" }));
+
+app.use(express.json());
 app.use("/auth", router);
+
 app.get("/", (req, res) => {
     res.send("<h1>Welcome Khaleed, how ya .........</h1>");
 });
+
 
 export const handler = serverless(app);
